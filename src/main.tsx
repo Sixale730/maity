0111218eb65c9e6f6d0173be12b6c2efa780c968
Sixale <jagv.1390@hotmail.com>
2025-09-22 12:04:28 -0600

    fix auth 1

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,16 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Canonical redirect: fuerza siempre www.maity.com.mx
+const CANON = "www.maity.com.mx";
+if (location.hostname !== CANON) {
+  location.replace(`https://${CANON}${location.pathname}${location.search}${location.hash}`);
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
